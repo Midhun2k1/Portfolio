8ee4e2ad76ac07349bfb021e12caa375b721bb06
Midhun2k1 <midhunovm@gmail.com>
2025-07-30 23:25:15 +0530

    Navbar update

--- my-porfolio/src/pages/Home.jsx
+++ my-porfolio/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
-/*import { HeroSection } from "../components/HeroSection";
+import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
-import { SkillsSection } from "../components/SkillsSection";
+/*import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer"; */
@@ -17,15 +17,12 @@ export const Home = () => {
       <StarBackground />
 
       {/* Navbar */}
-      {/* <Navbar /> */}
+      <Navbar />
       {/* Main Content */}
-      {/*<main>
+      <main>
         <HeroSection />
         <AboutSection />
-        <SkillsSection />
-        <ProjectsSection />
-        <ContactSection /> 
-      </main> */}
+      </main> 
 
       {/* Footer */}
       {/* <Footer /> */}
