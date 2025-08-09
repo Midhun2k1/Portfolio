119ab017f75b1fdda57ea5a9e58a1691f43024d9
Midhun2k1 <midhunovm@gmail.com>
2025-08-09 21:38:23 +0530

    Hero section update

--- my-porfolio/src/components/HeroSection.jsx
+++ my-porfolio/src/components/HeroSection.jsx
@@ -21,9 +21,10 @@ export const HeroSection = () => {
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            I create stellar web experiences with modern technologies.
-            Specializing in front-end development, I build interfaces that are
-            both beautiful and functional.
+            I craft scalable web applications with clean, 
+            efficient backends and dynamic front-end experiences. 
+            With a strong focus on backend development, 
+            I turn complex logic into scalable and efficient systems.
           </p>
 
           <div className="pt-4 opacity-0 animate-fade-in-delay-4">
