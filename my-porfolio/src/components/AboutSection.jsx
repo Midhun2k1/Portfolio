ea92fe10a2b975102824902b3f72b264151e59f1
Midhun2k1 <midhunovm@gmail.com>
2025-10-16 15:21:33 +0530

    resume update

--- my-porfolio/src/components/AboutSection.jsx
+++ my-porfolio/src/components/AboutSection.jsx
@@ -32,7 +32,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1dOuH2aeLMQy0v5-8SqHbh1FJzI6-e6iB/view?usp=drive_link"
+                // href="https://drive.google.com/file/d/1dOuH2aeLMQy0v5-8SqHbh1FJzI6-e6iB/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1g7qsnvsGkoXHi67NDzc1oLCKhde42dia/view"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
